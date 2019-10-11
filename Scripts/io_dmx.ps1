15caeee48aea955384efc1ca7d8b47ff5cd3f2c8
io-dmx <itchyo.dmx@gmail.com>
2019-10-10 20:31:48 -0600

    intial commit for scripts

---
+++ Scripts/io_dmx.ps1
@@ -0,0 +1,3 @@
+Start-Process "C:\QLC+\qlcplus.exe" -ArgumentList '-o "C:\Users\itchy-O\Documents\GitHub\io_dmx\QLC+\Workspaces\Itchy-O - LENOVO-APC20-MACRO.qxw" -f -w'
+
+Start-Process "C:\Program Files (x86)\Notepad++\notepad++.exe" -ArgumentList "C:\Users\itchy-O\Documents\setlist.txt  -alwaysOnTop"
