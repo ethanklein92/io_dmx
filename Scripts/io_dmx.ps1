f28feb048928146c3c59238f13599cec6d598f89
io-dmx <itchyo.dmx@gmail.com>
2020-02-25 19:02:06 -0700

    updated filename in start script

--- Scripts/io_dmx.ps1
+++ Scripts/io_dmx.ps1
@@ -1,3 +1,3 @@
-Start-Process "C:\QLC+\qlcplus.exe" -ArgumentList '-o "C:\Users\itchy-O\Documents\GitHub\io_dmx\QLC+\Workspaces\Itchy-O-LENOVO-4HATS-120819.qxw" -w'
+Start-Process "C:\QLC+\qlcplus.exe" -ArgumentList '-o "C:\Users\itchy-O\Documents\GitHub\io_dmx\QLC+\Workspaces\Itchy-O-LENOVO-021820.qxw" -w'
 
 Start-Process "C:\Program Files (x86)\Notepad++\notepad++.exe" -ArgumentList "C:\Users\itchy-O\Documents\setlist.txt  -alwaysOnTop"
